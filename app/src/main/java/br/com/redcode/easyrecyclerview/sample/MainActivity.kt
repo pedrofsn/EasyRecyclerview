967c63ccdf91c1e4eb87a6f88e6ee523cc7b022c
mikhail <goodlost@yandex.ru>
2019-10-22 00:56:52 +0300

    Revert "xml values refactoring"
    This reverts commit 0407dc92ca3a65fb8a681a43e869fd456c90fb19.

--- app/src/main/java/br/com/redcode/easyrecyclerview/sample/MainActivity.kt
+++ app/src/main/java/br/com/redcode/easyrecyclerview/sample/MainActivity.kt
@@ -9,16 +9,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-   private val click = { name: String, index: Int -> onClickItem(name, index) }
+    private val names = listOf("Pedro", "Luatane", "João Pedro", "José").sortedBy { it }
+    private val click = { name: String, index: Int -> onClickItem(name, index) }
     private val adapter by lazy { AdapterString(click) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val items = resources.getStringArray(R.array.sample_names)
-        val names:ArrayList<String> = ArrayList()
-        names.addAll(items)
 
         adapter.setCustomList(names)
         recyclerView.setCustomAdapter(adapter)
@@ -27,7 +24,7 @@ class MainActivity : AppCompatActivity() {
     private fun onClickItem(name: String, index: Int) {
         AlertDialog.Builder(this)
             .setTitle(name)
-            .setMessage(resources.getString(R.string.clicked_d, index))
+            .setMessage("Clicked in position $index")
             .setCancelable(false)
             .setPositiveButton(getString(android.R.string.ok)) { dialog, _ -> dialog?.dismiss() }
             .show()
